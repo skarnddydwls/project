b48753ad5bcd361aaba88205d07fe276fe370cbd
남궁용진 <elelte991@tukorea.ac.kr>
2025-11-24 21:47:46 +0900

    ㅎㅇ

---
+++ project/src/App.js
@@ -0,0 +1,38 @@
+import './App.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import { useEffect, useState } from 'react';
+import {Container, Nav, Navbar, Row, Col, Button} from 'react-bootstrap';
+import Signin from './pages/Signin';
+import Signup from './pages/Signup';
+
+function App() {
+  const [tab, setTab] = useState(null);
+  
+  return (
+    <>
+      <Navbar bg="dark" data-bs-theme="dark" style={{height: '80px'}}>
+        <Container style={{fontSize: '20px'}}>
+          <Nav>
+            <Nav.Link href="">경제</Nav.Link>
+            <Nav.Link href="">정치</Nav.Link>
+            <Nav.Link href="">사회</Nav.Link>
+            <Nav.Link href="">IT</Nav.Link>
+            <Nav.Link href="">연예</Nav.Link>
+          </Nav>
+          <Nav className='ms-auto'>
+            <Nav.Link onClick={()=>{setTab(tab === 'Signin' ? null : 'Signin')}}>로그인</Nav.Link>
+            <Nav.Link onClick={()=>{setTab(tab === 'Signup' ? null : 'Signup')}}>회원가입</Nav.Link>
+          </Nav>
+        </Container>
+      </Navbar>
+      <br/><br/><br/>
+
+      <Container>
+        { tab === 'Signin' ? <Signin/> : null }
+        { tab === 'Signup' ? <Signup/> : null }
+      </Container>
+    </>
+  );
+}
+
+export default App;
--- project/src/pages/Signup.js
+++ project/src/pages/Signup.js
@@ -1,52 +1,168 @@
-                    <Col sm="10">
-                    <Form.Control name="phone" onChange={handleChange} />
-                    </Col>
-                </Form.Group>
+import {Button, Col, Form, Modal, Row} from 'react-bootstrap';
+import React, { useState } from 'react';
+import axios from 'axios';
+import DaumPostCode from 'react-daum-postcode';
 
-                <Form.Group as={Row} className="mb-3" >
-                    <Form.Label column sm="2">ADDRESS</Form.Label>
-                    <Col sm="8">
-                    <Form.Control name="address" value={form.address} readOnly />
+const Signup = (() => {
+
+    const [form, setForm] = useState({
+        email: '',
+        password: '',
+        name: '',
+        birthday: '',
+        gender: '',
+        phone: '',
+        address: '',
+        detailAddress: ''
+    });
+
+
+    const handleChange = (e) => {
+        const {name, value} = e.target;
+        setForm({
+            ...form,
+            [name] : value
+        })
+        if(name === 'email') {
+            setEmailCheckMessage('');
+            setIsEmailChecked(false);
+        }
+    } 
+
+    const [emailCheckMessage, setEmailCheckMessage] = useState('');
+    const [isEmailChecked, setIsEmailChecked] = useState(false);
+    const [showPostCode, setShowPostCode] = useState(false);
+    
+    const checkEmail = () => {
+        axios.get('/react/email-check', {params: {email : form.email}})
+             .then(result => {
+                if(result.data) {
+                    setEmailCheckMessage('사용할 수 있는 이메일입니다');
+                    setIsEmailChecked(true);
+                } else {
+                    setEmailCheckMessage('사용할 수 없는 이메일입니다');
+                    setIsEmailChecked(false);
+                }
+             })
+             .catch(() => {
+                alert('이메일 확인 중 오류 발생!!!');
+             })
+    }
+
+    const handleComplete = (data) => {
+        setForm({...form, address: data.address})
+        setShowPostCode(false);
+    }
+
+    const handleSubmit = (e) => {
+        e.preventDefault();
+        if(isEmailChecked !== true){
+            alert("이메일 중복 확인을 해주세요")
+            return;
+        }
+        axios.post('/react/signup', form)
+             .then(() => {
+                alert("회원가입 성공")
+                window.location.href='/';
+             })
+             .catch (() => alert("회원가입 실패띠"))
+    }
+    return(
+        <div>
+            <h2>회원가입</h2>
+            <Form onSubmit={handleSubmit}>
+                <Form.Group as={Row} className="mb-3" controlId="email">
+                    <Form.Label column sm="2">Email</Form.Label>
+                    <Col sm="6">
+                    <Form.Control placeholder="elelte991@gmail.com" onChange={handleChange} name="email"/>
                     </Col>
+
                     <Col sm="2">
-                    <Button onClick={() => setShowPostcode(true)}>주소 검색</Button>
+                    <Button onClick={checkEmail}>중복확인</Button>
+                    </Col>
+                    {
+                        emailCheckMessage && (
+                            <span style={{color: isEmailChecked ? 'green' : 'red', fontSize: '0.6em', marginTop: '5px'}}>
+                                {emailCheckMessage}
+                            </span>
+                        )
+                    }
+                </Form.Group>
+
+                <Form.Group as={Row} className="mb-3" controlId="Password">
+                    <Form.Label column sm="2">Password</Form.Label>
+                    <Col sm="6">
+                    <Form.Control type="password" placeholder="Password" name="password" onChange={handleChange}/>
                     </Col>
                 </Form.Group>
 
-                <Form.Group as={Row} className="mb-3" >
-                    <Form.Label column sm="2">DETAIL ADDRESS</Form.Label>
-                    <Col sm="10">
-                    <Form.Control name="detailAddress" onChange={handleChange}/>
+                <Form.Group as={Row} className="mb-3" controlId="Name">
+                    <Form.Label column sm="2">Name</Form.Label>
+                    <Col sm="6">
+                    <Form.Control placeholder="Name" name="name" onChange={handleChange}/>
                     </Col>
                 </Form.Group>
-                <Button type="submit">회원가입</Button>
-            </Form>
 
-            <Modal show={showPostcode} onHide={() => setShowPostcode(false)}>
-                <Modal.Header closeButton>
+                <Form.Group as={Row} className="mb-3" controlId="Birthday">
+                    <Form.Label column sm="2">Birthday</Form.Label>
+                    <Col sm="6">
+                    <Form.Control placeholder="Birthday" type="date" name="birthday" onChange={handleChange}/>
+                    </Col>
+                </Form.Group>
+
+                <Form.Group as={Row} className="mb-3" controlId="Gender">
+                    <Form.Label column sm="2">Gender</Form.Label>
+                    <Col sm="6">
+                    <Form.Select name="gender" onChange={handleChange}>
+                        <option>성별</option>
+                        <option value="남자">남자</option>
+                        <option value="여자">여자</option>
+                    </Form.Select>
+                    </Col>
+                </Form.Group>
+
+                <Form.Group as={Row} className="mb-3" controlId="Phone">
+                    <Form.Label column sm="2">Phone</Form.Label>
+                    <Col sm="6">
+                    <Form.Control placeholder="Phone" name="phone" onChange={handleChange}/>
+                    </Col>
+                </Form.Group>
+
+                <Form.Group as={Row} className="mb-3" controlId="Address">
+                    <Form.Label column sm="2">Address</Form.Label>
+                    <Col sm="6">
+                    <Form.Control placeholder="Address" name="address" value={form.address} readOnly />
+                    </Col>
+
+                    <Col sm="2">
+                    <Button onClick={() => setShowPostCode(true)}>주소 검색</Button>
+                    </Col>
+                </Form.Group>
+
+                <Form.Group as={Row} className="mb-3" controlId="detailAddress">
+                    <Form.Label column sm="2">DetailAddress</Form.Label>
+                    <Col sm="6">
+                    <Form.Control placeholder="detailAddress" name="detailAddress" onChange={handleChange}/>
+                    </Col>
+                </Form.Group>
+                
+                <Form.Group as={Row} className="mb-3" controlId="submit">
+                    <Col sm="8">
+                    <Button type='submit'>회원가입</Button>
+                    </Col>
+                </Form.Group>
+
+                <Modal show={showPostCode} onHide={() => setShowPostCode(false)}>
+                    <Modal.Header closeButton>
                     <Modal.Title>주소 검색</Modal.Title>
-                </Modal.Header>
-                <Modal.Body>
-                    <DaumPostcode onComplete={handleComplete}/>
-                </Modal.Body>
-            </Modal>
+                    </Modal.Header>
+                    <Modal.Body>
+                        <DaumPostCode onComplete={handleComplete}/>
+                    </Modal.Body>
+                </Modal>
+            </Form>
         </div>
     )
-}
-
-const style = {
-    container : {
-        textAlign : 'center',
-        marginTop : '40px',
-    },
-    title : {
-        textAlign: 'center',
-        marginBottom : '20px'
-    },
-    form : {
-        margin: '0 auto',
-        width : '80%'
-    }
-}
+})
 
 export default Signup;
