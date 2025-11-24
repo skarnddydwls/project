207dac1bd0a383fee69df3d51bfe8731b7e95baf
남궁용진 <elelte991@tukorea.ac.kr>
2025-11-24 20:22:37 +0900

    hi

--- project/src/pages/Signin.js
+++ project/src/pages/Signin.js
@@ -1,4 +1,40 @@
+import {Button, Col, Form, Row} from 'react-bootstrap';
+import { useState } from 'react';
+import axios from 'axios';
+import 'bootstrap/dist/css/bootstrap.min.css';
+
 const Signin = () => {
+    const [form, setForm] = useState ({
+            email: '',
+            password: ''
+        }
+    )
+
+    const handleChange = (e) => {
+        const {name, value} = e.target;
+        setForm({...form, [name] : value});
+    }
+
+    const handleSubmit = (e) => {
+        e.preventDefault(); // 서버로 가는거 막아줌
+        axios.post('/react/login', form)
+             .then(result => {
+                if(result.data) {
+                    alert('로그인 되었습니다');
+                    const userInfo = {
+                        name: result.data.name,
+                        email: result.data.email
+                    }
+                    sessionStorage.setItem('loginUser', JSON.stringify(userInfo));
+                    window.location.href='/';
+                } else {
+                    alert('이메일 또는 비밀번호가 일치하지 않습니다')
+                }
+             })
+             .catch(() => {
+                alert('로그인에 실패하였습니다'); // 서버 측 오류
+             })
+    }
     return(
         <>
             <Form onSubmit={handleSubmit}>
