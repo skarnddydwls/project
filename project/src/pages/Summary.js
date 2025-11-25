1cea6dfdaa31bd28fef8994ef99a93d2064ac060
남궁용진 <elelte991@tukorea.ac.kr>
2025-11-25 20:03:06 +0900

    상세페이지

---
+++ project/src/pages/Summary.js
@@ -0,0 +1,15 @@
+import { useParams } from 'react-router-dom';
+import { useEffect } from 'react';
+import axios from 'axios';
+
+const Summary = () => {
+    const {category, id} = useParams();
+
+    return(
+        <>
+
+        </>
+    )
+}
+
+export default Summary;
