eb9b7f956ee9c683dfc5d6adff5555ca7348dcce
남궁용진 <elelte991@tukorea.ac.kr>
2025-12-09 16:43:34 +0900

    더 보기 버튼 추가

--- project/src/pages/NewsCategory.js
+++ project/src/pages/NewsCategory.js
@@ -3,20 +3,25 @@ import axios from 'axios';
 import { Row, Col, Container } from 'react-bootstrap';
 import {useNavigate, useParams} from 'react-router-dom'
 import '../css/page.css';
+import Button from 'react-bootstrap/Button';
 
 
 const NewsCategory = () => {
     const {category} = useParams();
     const [newsList, setNewsList] = useState([]);
+    const [limit, setLimit] = useState(5);
+
+
     let navigate = useNavigate();
 
     useEffect(() => {
         axios
         .get(`/api/article/${category}`)
-        .then(res => setNewsList(res.data))
+        .then((res) => {
+            setNewsList(res.data)
+        })
         .catch(err => console.error(err));
 
-        console.log(`newsCategory: ${sessionStorage.getItem('loginUser')}`)
     }, [category]);
 
         return (
@@ -25,20 +30,28 @@ const NewsCategory = () => {
             <Col >
                 <h2 className="section-title">{category}</h2>
 
-                {newsList.map((news) => (
+                {newsList.slice(0,limit).map((news) => (
                 <Row key={news.id} className="news-row">
                     <Col md={6} xs={3}>
                     <h3 onClick={()=> {navigate(`/${category}/News/${news.articleId}`)}} className="newsTitle">{news.title}</h3>
                     </Col>
                     <Col md={3} xs={4}>
                     <img
-                        src={"https://mimgnews.pstatic.net/image/origin/008/2025/12/05/5288022.jpg?type=nf220_150"}
+                        src={news.img}
                         alt={news.title}
                         className="news-thumb"
                     />
                     </Col>
                 </Row>
                 ))}
+
+                {limit < newsList.length && ( // limit가 list 크기보다 작을 때만 보여짐
+                    <div className="d-grid mt-4">
+                        <Button variant="secondary" size="lg" onClick={() => setLimit(limit + 5)}>
+                            더 보기
+                        </Button>
+                    </div>
+                )}
             </Col>
             </Row>
         </Container>
